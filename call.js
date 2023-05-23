b09732fad444bc8336867a02405a29adf5d6206c
beauwright <93743705+beauwright@users.noreply.github.com>
2023-05-23 17:35:20 -0600

    Refactored call.js and added screenshare and mic audio capture, added setup modals

--- call.js
+++ call.js
@@ -1,153 +1,233 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let notificationWords = ['Hank'];  // Store notification words in an array
-    let dictionaryWords = [];  // Store dictionary words in an array
+class Transcript {
+    constructor() {
+        this.title = document.getElementById('transcript-title');
+        this.title.addEventListener('click', () => $('#changeTitleModal').modal('show'));
+        document.getElementById('saveTitle').addEventListener('click', this.saveTitle.bind(this));
 
-    // Set initial title
-    const date = new Date();
-    const transcriptTitle = document.getElementById('transcript-title');
-    transcriptTitle.textContent = `${(date.getMonth() + 1)}/${date.getDate()}/${date.getFullYear().toString().slice(-2)} ${date.toLocaleTimeString()} Transcript`;
+        this.notificationWords = ['Hank'];
+        this.dictionaryWords = [];
+        this.setupModalListeners();
+        this.initEnvironmentCheck();
+        this.setupScreenShare();
+    }
 
-    // Handle clicks on the title
-    transcriptTitle.addEventListener('click', function() {
-        $('#changeTitleModal').modal('show');
-    });
 
-    // Save the new title
-    const saveTitleButton = document.getElementById('saveTitle');
-    saveTitleButton.addEventListener('click', function() {
+    initEnvironmentCheck() {
+        let browser, os;
+        const userAgent = navigator.userAgent;
+
+        if (userAgent.includes("Chrome") && !userAgent.includes("Edg")) {
+            browser = "Chrome";
+        } else if (userAgent.includes("Edg")) {
+            browser = "Edge";
+        }
+
+        if (navigator.platform.includes("Win")) {
+            os = "Windows";
+        } else if (navigator.platform.includes("Mac")) {
+            os = "MacOS";
+        } else if (navigator.platform.includes("Linux")) {
+            os = "Linux";
+        }
+
+        // Show the modal with appropriate message
+        if (browser === "Chrome" || browser === "Edge") {
+            if (os === "Windows" || os === "ChromeOS") {
+                // User should screen share their audio
+                $('#initCheckModal .modal-body').text(`Screen sharing is used to analyze the audio of your call. Ensure the "share desktop audio" box is checked so Call Sidekick can analyze the audio of your call.`);
+                $('#initCheckModal .modal-footer').html(`<button id="startScreenShare" class="btn btn-primary">Start Screen Share</button>
+                                                         <button id="bypassScreenShare" class="btn btn-secondary">Bypass Screen Share</button>`);
+            } else if (os === "MacOS" || os === "Linux") {
+                // User should screen share their audio and use web version of their video call service
+                $('#initCheckModal .modal-body').text(`${browser} currently only supports audio capture of other ${browser} tabs on ${os}. Most video call platforms like Zoom, Google Meet, and Discord have web versions. Open your video call in your web browser and screen share that tab with the "share tab audio" box checked so Call Sidekick can analyze the audio of your call.`);
+                $('#initCheckModal .modal-footer').html(`<button id="startScreenShare" class="btn btn-primary">Start Screen Share</button>
+                                                         <button id="bypassScreenShare" class="btn btn-secondary">Bypass Screen Share</button>`);
+            }
+        } else {
+            // Browser not supported
+            $('#initCheckModal .modal-body').text(`You are using an unsupported browser. Only Chrome and Edge are currently supported.`);
+            $('#initCheckModal .modal-footer').html(`<button class="btn btn-secondary" data-dismiss="modal">Close</button>`);
+        }
+
+        $('#initCheckModal').modal('show');
+    }
+
+    setupScreenShare() {
+        document.getElementById('startScreenShare').addEventListener('click', this.startScreenShare.bind(this));
+        document.getElementById('bypassScreenShare').addEventListener('click', this.bypassScreenShare.bind(this));
+    }
+
+    async startScreenShare() {
+        try {
+            const displayMediaOptions = {
+                video: {
+                    cursor: "always"
+                },
+                audio: {
+                    echoCancellation: true,
+                    noiseSuppression: true,
+                    sampleRate: 44100
+                }
+            };
+
+            const stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+            // Handle the audio stream for further use (e.g., sending it to the server)
+            $('#initCheckModal').modal('hide');
+            // Request microphone permissions after closing the screen share modal
+            this.requestMicrophonePermissions();
+        } catch(err) {
+            console.error("Error: " + err);
+        }
+    }
+
+    bypassScreenShare() {
+        // Close the initial check modal
+        $('#initCheckModal').modal('hide');
+        // Open a new modal for microphone permissions
+        this.requestMicrophonePermissions();
+    }
+
+    requestMicrophonePermissions() {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(stream => {
+                this.audioStream = stream;
+                this.initiateTranscript();
+                $('#micPermissionModal').modal('hide');
+            })
+            .catch(err => {
+                // Handling the error appropriately.
+                console.log('Microphone permission denied', err);
+                $('#micPermissionModal .modal-body').text(`Microphone permission was denied. Please, allow access to continue.`);
+                $('#micPermissionModal .modal-footer').html(`<button class="btn btn-primary" id="retryPermissions">Retry Permissions</button>
+                                                     <button class="btn btn-secondary" data-dismiss="modal">Close</button>`);
+                document.getElementById('retryPermissions').addEventListener('click', this.requestMicrophonePermissions.bind(this));
+                // Show the microphone permission modal
+                $('#micPermissionModal').modal('show');
+            });
+    }    
+    setInitialTitle() {
+        const date = new Date();
+        this.title.textContent = `${(date.getMonth() + 1)}/${date.getDate()}/${date.getFullYear().toString().slice(-2)} ${date.toLocaleTimeString()} Transcript`;
+    }
+
+    saveTitle() {
         const newTitle = document.getElementById('newTitle').value;
         if (newTitle.trim() !== '') {
-            transcriptTitle.textContent = newTitle;
+            this.title.textContent = newTitle;
             $('#changeTitleModal').modal('hide');
         }
-    });
+    }
 
-    // Function to add a word
-    function addWord(word, containerId, wordArray) {
-        // Create a new word element
+    setupModalListeners() {
+        $('#transcriptSettingsModal').on('show.bs.modal', this.populateModal.bind(this));
+    }
+
+    populateModal() {
+        const containers = {
+            notification: document.getElementById('notificationWordsContainer'),
+            dictionary: document.getElementById('dictionaryWordsContainer')
+        };
+
+        containers.notification.innerHTML = '';
+        containers.dictionary.innerHTML = '';
+
+        const addButtons = {
+            notification: document.getElementById('addNotificationWord'),
+            dictionary: document.getElementById('addDictionaryWord')
+        };
+
+        const newWordInputs = {
+            notification: document.getElementById('newNotificationWord'),
+            dictionary: document.getElementById('newDictionaryWord')
+        };
+
+        this.addWordListeners(addButtons, newWordInputs, containers);
+        this.populateWordContainers(containers);
+    }
+
+    addWordListeners(addButtons, newWordInputs, containers) {
+        addButtons.notification.addEventListener('click', () => this.addNewWord(newWordInputs.notification, this.notificationWords, containers.notification));
+        addButtons.dictionary.addEventListener('click', () => this.addNewWord(newWordInputs.dictionary, this.dictionaryWords, containers.dictionary));
+    }
+
+    addNewWord(newWordInput, wordArray, container) {
+        const newWord = newWordInput.value;
+        if (newWord !== '' && !wordArray.includes(newWord)) {
+            wordArray.push(newWord);
+            this.addWord(newWord, container, wordArray);
+            newWordInput.value = '';
+        }
+    }
+
+    addWord(word, container, wordArray) {
         const wordElement = document.createElement('div');
         wordElement.classList.add('mb-2', 'd-flex', 'justify-content-between', 'align-items-center');
-
-        // Add the word text
-        const wordText = document.createElement('span');
-        wordText.textContent = word;
-        wordElement.appendChild(wordText);
-
-        // Add the remove button
-        const removeButton = document.createElement('button');
-        removeButton.textContent = 'Remove';
-        removeButton.classList.add('btn', 'btn-danger');
-        removeButton.addEventListener('click', function() {
-            // Remove the word from the array
-            const index = wordArray.indexOf(word);
-            if (index > -1) {
-                wordArray.splice(index, 1);
-            }
-
-            // Remove the element from the DOM
-            wordElement.remove();
-        });
-        wordElement.appendChild(removeButton);
-
-        // Add the word element to the modal body
-        const container = document.getElementById(containerId);
+        wordElement.innerHTML = `
+            <span>${word}</span>
+            <button class="btn btn-danger">Remove</button>
+        `;
+        wordElement.querySelector('button').addEventListener('click', () => this.removeWord(word, wordElement, wordArray));
         container.appendChild(wordElement);
     }
 
-    // Handle opening of the transcript settings modal
-    $('#transcriptSettingsModal').on('show.bs.modal', function () {
-        const notificationContainer = document.getElementById('notificationWordsContainer');
-        const dictionaryContainer = document.getElementById('dictionaryWordsContainer');
-        notificationContainer.innerHTML = '';  // Clear existing words
-        dictionaryContainer.innerHTML = '';  // Clear existing words
-
-        const newNotificationWord = document.getElementById('newNotificationWord');
-        const addNotificationWordButton = document.getElementById('addNotificationWord');
-        const newDictionaryWord = document.getElementById('newDictionaryWord');
-        const addDictionaryWordButton = document.getElementById('addDictionaryWord');
-
-        addNotificationWordButton.addEventListener('click', function() {
-            const newWord = newNotificationWord.value;
-            if (newWord !== '' && !notificationWords.includes(newWord)) {
-                notificationWords.push(newWord);
-                addWord(newWord, 'notificationWordsContainer', notificationWords);
-                newNotificationWord.value = '';  // Clear the input field
-            }
-        });
-
-        addDictionaryWordButton.addEventListener('click', function() {
-            const newWord = newDictionaryWord.value;
-            if (newWord !== '' && !dictionaryWords.includes(newWord)) {
-                dictionaryWords.push(newWord);
-                addWord(newWord, 'dictionaryWordsContainer', dictionaryWords);
-                newDictionaryWord.value = '';  // Clear the input field
-            }
-        });
-
-        // Add each word
-        for (const word of notificationWords) {
-            addWord(word, 'notificationWordsContainer', notificationWords);
+    removeWord(word, wordElement, wordArray) {
+        const index = wordArray.indexOf(word);
+        if (index > -1) {
+            wordArray.splice(index, 1);
         }
+        wordElement.remove();
+    }
 
-        for (const word of dictionaryWords) {
-            addWord(word, 'dictionaryWordsContainer', dictionaryWords);
+    populateWordContainers(containers) {
+        for (const word of this.notificationWords) {
+            this.addWord(word, containers.notification, this.notificationWords);
         }
-    });
+        for (const word of this.dictionaryWords) {
+            this.addWord(word, containers.dictionary, this.dictionaryWords);
+        }
+    }
 
+    initiateTranscript() {
+        console.log('Initiate transcript');
+        // Additional code here...
+    }
+}
 
-    function saveNote() {
-        // Get the note text
-        const noteText = document.getElementById('new-note').value;
+class Notes {
+    constructor() {
+        this.container = document.getElementById('existing-notes');
+        document.getElementById('save-note').addEventListener('click', this.saveNote.bind(this));
+        document.getElementById('end-transcript').addEventListener('click', this.endTranscript.bind(this));
+    }
 
-        // Check if the note is not empty
-        if (noteText.trim() !== '') {
-            // Create timestamp
-            const date = new Date();
-            const timestamp = date.toLocaleTimeString();
+    saveNote() {
+        const newNote = document.getElementById('new-note').value;
+        if (newNote.trim() !== '') {
+            const noteElement = document.createElement('div');
+            noteElement.classList.add('note-item');
 
-            // Create a new note element
-            const noteElement = document.createElement('p');
-            noteElement.textContent = timestamp + ': ' + noteText;
+            const noteTime = new Date();
+            const noteTimeString = noteTime.toLocaleTimeString();
 
-            // Add the note element to the existing notes
-            const existingNotes = document.getElementById('existing-notes');
-            existingNotes.appendChild(noteElement);
+            // Store both note and time in the element's innerHTML
+            noteElement.innerHTML = `<span class="note-time">${noteTimeString}</span> ${newNote}`;
 
-            // Clear the note input
+            this.container.appendChild(noteElement);
             document.getElementById('new-note').value = '';
+            this.container.style.display = 'block';
 
-            // Scroll to the bottom
-            noteElement.scrollIntoView();
-
-            checkNoteExistence();
+            // Scroll the container to the bottom
+            this.container.scrollTop = this.container.scrollHeight;
         }
     }
 
-    function checkNoteExistence() {
-        const existingNotes = document.getElementById('existing-notes');
-        const noteElements = existingNotes.getElementsByTagName('p');
-
-        if (noteElements.length === 0) {
-            existingNotes.style.display = 'none';
-        } else {
-            existingNotes.style.display = 'block';
-        }
-    }
-
-    // Attach the save note function to the save note button
-    const saveNoteButton = document.getElementById('save-note');
-    saveNoteButton.addEventListener('click', saveNote);
-    // Get the "End Transcript" button
-    const endTranscriptButton = document.getElementById('end-transcript');
-
-    // Listen for click events on the "End Transcript" button
-    endTranscriptButton.addEventListener('click', function() {
-        // Implement server functionality here...
-
-        // Redirect to dashboard.html
+    endTranscript() {
         window.location.href = 'dashboard.html';
-    });
+    }
+}
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    const transcript = new Transcript();
+    new Notes();
+    transcript.setInitialTitle();
 });
-
-
